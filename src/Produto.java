6e96a835f5ab96044af16fac9472f188685e5df9
estevao7k <estevao.oliveiratech@gmail.com>
2025-10-06 00:36:27 -0300

    encapsulamento da classe Produto.java e criacao da funcao CsvString para transformar dados primitivos em String e retornar ";" com String.join.

--- src/Produto.java
+++ src/Produto.java
@@ -2,17 +2,19 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Produto {
-    String Codigo;
-    String Nome;
-    String Descricao;
-    LocalDate DataFabricacao;
-    LocalDate DataValidade;
-    BigDecimal PrecoCompra;
-    BigDecimal PrecoVenda;
-    int QuantidadeEstoque;
-    Categoria Categoria;
+    private String Codigo;
+    private String Nome;
+    private String Descricao;
+    private LocalDate DataFabricacao;
+    private LocalDate DataValidade;
+    private BigDecimal PrecoCompra;
+    private BigDecimal PrecoVenda;
+    private int QuantidadeEstoque;
+    private Categoria Categoria;
 
-    public Produto (String Codigo, String Nome, String Descricao, LocalDate DataFabricacao, LocalDate DataValidade, BigDecimal PrecoCompra, BigDecimal PrecoVenda, int QuantidadeEstoque, Categoria Categoria){
+    public Produto(String Codigo, String Nome, String Descricao, LocalDate DataFabricacao,
+                   LocalDate DataValidade, BigDecimal PrecoCompra, BigDecimal PrecoVenda,
+                   int QuantidadeEstoque, Categoria Categoria) {
         this.Codigo = Codigo;
         this.Nome = Nome;
         this.Descricao = Descricao;
@@ -22,5 +24,32 @@ public class Produto {
         this.PrecoVenda = PrecoVenda;
         this.QuantidadeEstoque = QuantidadeEstoque;
         this.Categoria = Categoria;
+    }
+
+    public String getCodigo() { return this.Codigo; }
+    public String getNome() { return this.Nome; }
+    public String getDescricao() { return this.Descricao; }
+    public LocalDate getDataFabricacao() { return this.DataFabricacao; }
+    public LocalDate getDataValidade() { return this.DataValidade; }
+    public BigDecimal getPrecoCompra() { return this.PrecoCompra; }
+    public BigDecimal getPrecoVenda() { return this.PrecoVenda; }
+    public int getQuantidadeEstoque() { return this.QuantidadeEstoque; }
+    public Categoria getCategoria() { return this.Categoria; }
+
+    public String CsvString() {
+        return String.join(";",
+                this.Codigo,
+                this.Nome,
+                this.Descricao,
+                this.DataFabricacao.toString(),
+                this.DataValidade.toString(),
+                this.PrecoCompra.toString(),
+                this.PrecoVenda.toString(),
+                Integer.toString(this.QuantidadeEstoque),
+                Integer.toString(this.Categoria.getId()),
+                this.Categoria.getNomeProduto(),
+                this.Categoria.getDescricao(),
+                this.Categoria.getSetor()
+        );
     }
 }
