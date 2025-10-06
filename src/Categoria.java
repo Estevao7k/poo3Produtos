922ab64b319b6305fb814e0ef1893f997a8a1964
estevao7k <estevao.oliveiratech@gmail.com>
2025-10-05 22:46:05 -0300

    Initial commit

---
+++ src/Categoria.java
@@ -0,0 +1,13 @@
+public class Categoria {
+        int Id;
+        String NomeProduto;
+        String Descricao;
+        String Setor;
+
+        public Categoria (int id, String NomeProduto, String Descricao, String Setor) {
+            this.Id = Id;
+            this.NomeProduto = NomeProduto;
+            this.Descricao = Descricao;
+            this.Setor = Setor;
+        }
+}
