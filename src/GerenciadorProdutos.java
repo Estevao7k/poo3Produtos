07774014801a80f105594c4149eb5ea84ad40cef
estevao7k <estevao.oliveiratech@gmail.com>
2025-10-06 00:32:33 -0300

    foram criadas as funcoes de adicionar, listar e buscar em GerenciadorProdutos.java

--- src/GerenciadorProdutos.java
+++ src/GerenciadorProdutos.java
@@ -8,4 +8,23 @@ public class GerenciadorProdutos {
         this.ListaProdutos = new ArrayList<>();
     }
 
+    public void AdicionarProduto(Produto ProdutoParaAdicionar){
+        this.ListaProdutos.add(ProdutoParaAdicionar);
+
+    }
+
+    public List<Produto> ListarTodosProdutos(){
+        return this.ListaProdutos;
+    }
+
+    public Produto buscarProdutoPorCodigo(String CodigoParaBuscar){
+        for (Produto produto : this.ListaProdutos){
+            if (produto.getCodigo().equals(CodigoParaBuscar)){
+                return  produto;
+            }
+        }
+        return null;
+    }
+
+
 }
