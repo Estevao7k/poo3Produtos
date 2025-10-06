5612d689a9daefd45fd550226b730c23197b3953
estevao7k <estevao.oliveiratech@gmail.com>
2025-10-05 23:51:53 -0300

    criacao classe GerenciadorProdutos.java

---
+++ src/GerenciadorProdutos.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorProdutos {
+    private List<Produto> ListaProdutos;
+
+    public GerenciadorProdutos(){
+        this.ListaProdutos = new ArrayList<>();
+    }
+
+}
